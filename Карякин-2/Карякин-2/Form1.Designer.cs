ccad9af1635a0ac906b49fc31e9cba9ee9bfd4a9
StarkPopStar <48656681+StarkPopStar@users.noreply.github.com>
2019-04-29 20:11:37 +0500

    task 3

---
+++ Карякин-2/Карякин-2/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Карякин_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.VarComb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pronbability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.VarComb,
+            this.Pronbability});
+            this.dataGridView1.Location = new System.Drawing.Point(788, 48);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(436, 510);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // VarComb
+            // 
+            this.VarComb.HeaderText = "X";
+            this.VarComb.Name = "VarComb";
+            // 
+            // Pronbability
+            // 
+            this.Pronbability.HeaderText = "P";
+            this.Pronbability.Name = "Pronbability";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 252);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Энтропия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Первая переменная";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(313, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Вероятность первого события";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(411, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Вторая переменная";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(411, 123);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(311, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Вероятность второго события";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 283);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(314, 31);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 48);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(314, 31);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 154);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(314, 31);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(417, 48);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(312, 31);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(417, 154);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(312, 31);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 278);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(783, 17);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(375, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Таблица переменных и их значений";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1577, 708);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Вычисление энтропии";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VarComb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pronbability;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Карякин-2/Карякин-2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Карякин_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            textBox1.Clear();
+            if (textBox2.Text == "" || textBox3.Text == "" || textBox3.Text == "" ||
+               textBox4.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Не все поля заполнены!");
+                return;
+            }
+            textBox2.CharacterCasing = CharacterCasing.Lower;
+            string firstLetter = textBox2.Text;
+            string secondLetter = textBox4.Text;
+            double firstProbability = double.Parse(textBox3.Text.ToString());
+            double secondProbability = double.Parse(textBox5.Text.ToString());
+            HashSet<string> s = new HashSet<string>();
+            double[] combinationsP = { firstProbability, secondProbability };
+            string[] combinationsL = { firstLetter, secondLetter };
+            double H = 0;
+            double[] q = new double[4];
+            int j = 0;
+            IList<double> comboP = new List<double>();
+            IList<string> comboLetters = new List<string>();
+            for (int i = 0; i < combinationsP.Length; i++)
+            {
+                if (firstProbability < 0 || secondProbability > 1 || 
+                    firstProbability > 1 || secondProbability < 0)
+                {
+                    textBox1.Text = "";
+                    MessageBox.Show("Вероятность события должна быть больше или равна нулю и не должна быть больше единицы.");
+                    return;
+                }
+                if (firstLetter == secondLetter)
+                {
+                    MessageBox.Show("Названия переменных должны быть отличны друг от друга ");
+                    textBox1.Text = "";
+                    return;
+                }
+                if (firstProbability + secondProbability != 1)
+                {
+                    MessageBox.Show("Сумма вероятностей должна быть равна единице.");
+                    textBox1.Text = "";
+                    return;
+                }
+                for (int k = 0; k < combinationsP.Length; k++)
+                {
+                    q[j] = combinationsP[i] * combinationsP[k];
+                    comboP.Add(q[j]);
+                    j++;
+                }
+                j = 0;
+                
+            }
+            for (int i = 0; i < combinationsL.Length; i++)
+            {
+                for (int l = 0; l < combinationsL.Length; l++)
+                {
+                    string combination3 =  combinationsL[i] + combinationsL[l];
+                    comboLetters.Add(combination3);
+                    H += -q[j] * Math.Log(q[j], 4);
+                }
+            }
+            if (firstProbability == 0 || secondProbability == 0)
+            {
+                textBox1.Text = H.ToString();
+                for (int w = 0; w < comboP.Count; w++)
+                {
+                    dataGridView1.Rows.Add(comboLetters.ElementAt(w), comboP.ElementAt(w));
+                }
+                H = 0;
+                textBox1.Text = H.ToString();
+                return;
+            }
+            else
+            {
+                for (int w = 0; w < comboP.Count; w++)
+                {
+                    dataGridView1.Rows.Add(comboLetters.ElementAt(w), comboP.ElementAt(w));
+                }
+                textBox1.Text = Math.Round(H, 4).ToString();
+            }
+            
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if((e.KeyChar <=47 || e.KeyChar >=58) && number !=8 && number !=44)
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44)
+            {
+                e.Handled = true;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+   
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+  
+    }
+
+       
+        
+    
+}
