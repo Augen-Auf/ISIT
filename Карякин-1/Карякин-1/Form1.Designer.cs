a14bfaac26890b3b24ab75bd34846c2719b10143
StarkPopStar <48656681+StarkPopStar@users.noreply.github.com>
2019-04-29 20:09:34 +0500

    task 1

---
+++ Карякин-1/Карякин-1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Карякин_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.P});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(247, 243);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // P
+            // 
+            this.P.HeaderText = "P";
+            this.P.Name = "P";
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.LightGray;
+            this.result.Location = new System.Drawing.Point(307, 54);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(130, 25);
+            this.result.TabIndex = 1;
+            this.result.Text = "Рассчитать";
+            this.result.UseVisualStyleBackColor = false;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(130, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Энтропия";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 365);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Вычисление энтропии";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Карякин-1/Карякин-1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Карякин_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.Rows.Add(2);
+        }
+        private void result_Click(object sender, EventArgs e)
+        {
+           double H = 0;
+           double[] probability = new double[dataGridView1.RowCount];
+           double sumProbabilities = 0;
+           string x1; 
+           string x2;
+           for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value == null)
+                    {
+                        MessageBox.Show("Не все поля заполнены!");
+                        return;
+                    }
+                }
+                if (!double.TryParse(dataGridView1[1, i].Value.ToString(), out probability[i]))
+                {
+                    MessageBox.Show("Должны быть введены только числа.");
+                    return;
+                }
+                if (probability[i] < 0 || probability[i] > 1)
+                {
+                    MessageBox.Show("Вероятность события должна быть ни меньше нуля и ни больше единицы.");
+                    return;
+                }
+                if (probability[i] == 0)
+                {
+                    textBox1.Text = H.ToString();
+                    MessageBox.Show("Энтропия равна 0");
+                    return;
+                }
+                sumProbabilities += probability[i];
+            }
+            x1 = dataGridView1[0, 0].Value.ToString();
+            x2 = dataGridView1[0, 1].Value.ToString();
+            if (x1 == x2)
+            {
+                MessageBox.Show("Названия переменных должны быть отличны друг от друга");
+                return;
+            }
+            if (sumProbabilities == 1)
+                for (int i = 0; i < dataGridView1.RowCount ; i++)
+                {
+                    H += -probability[i] * Math.Log(probability[i], 2);
+                    textBox1.Text = Math.Round(H, 4).ToString();
+                }
+            else
+            {
+                MessageBox.Show("Сумма вероятностей должна быть равна единице.");
+            }
+        }
+    }
+}
